2262820c5a74a2aabbb3d56132927227dc863fe7
Victor Hong <victor@victorqhong.com>
2025-03-10 10:14:58 -0400

    Add AskQuestion tool

---
+++ Simulation/Tools/AskQuestion.cs
@@ -0,0 +1,71 @@
+namespace Simulation.Tools;
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+
+public class AskQuestion
+{
+    private JObject schema = JObject.FromObject(new
+    {
+        type = "function",
+        function = new
+        {
+            name = "question_ask",
+            description = "Ask a question to someone knowledgeable.",
+            parameters = new
+            {
+                type = "object",
+                properties = new
+                {
+                    question = new
+                    {
+                        type = "string",
+                        description = "The question you want to ask"
+                    }
+                },
+                required = new[] { "question" }
+            }
+        }
+    });
+
+    public AskQuestion()
+    {
+        Tool = new Tool
+        {
+            Schema = schema,
+            Function = Function
+        };
+    }
+
+    public Tool Tool { get; private set; }
+
+    private JToken Function(JObject parameters)
+    {
+        var result = new JObject();
+
+        var question = parameters["question"]?.ToString();
+        if (string.IsNullOrEmpty(question))
+        {
+            result.Add("error", "question is null or empty");
+            return result;
+        }
+
+        try
+        {
+            Console.WriteLine($"Question: {question}");
+            Console.Write("Answer> ");
+            var answer = Console.ReadLine();
+            result.Add("answer", answer);
+        }
+        catch (Exception e)
+        {
+            result.Add("exception", e.Message);
+        }
+
+        return result;
+    }
+}
+
+
