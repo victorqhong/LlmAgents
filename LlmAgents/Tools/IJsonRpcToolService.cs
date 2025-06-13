cbef2cca0a0038eb94e62046120d4da2a944a5aa
Victor Hong <victor@victorqhong.com>
2025-06-13 14:20:22 -0400

    Modify tool service interface

--- LlmAgents/Tools/IJsonRpcToolService.cs
+++ LlmAgents/Tools/IJsonRpcToolService.cs
@@ -2,7 +2,7 @@ namespace LlmAgents.Tools;
 
 public interface IJsonRpcToolService
 {
-    Task<string> CallTool(string name, string parameters);
-    Task<string> GetToolSchema(string name);
+    Task<string?> CallTool(string name, string parameters);
+    Task<string?> GetToolSchema(string name);
     Task<string[]> GetToolNames();
 }
