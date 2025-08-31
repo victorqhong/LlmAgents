9015b8e3d734f1e9febd3619c0a6918ef558f09c
Victor Hong <victor@victorqhong.com>
2025-08-31 11:57:31 -0400

    fix: update WebSearch tool to use Tavily

--- LlmAgents.Tools/WebSearch.cs
+++ LlmAgents.Tools/WebSearch.cs
@@ -7,19 +7,13 @@ public class WebSearch : Tool
 {
     private readonly string? apiKey;
 
-    private readonly string market;
-
-    private readonly string safeSearch;
-
     private readonly string baseUrl;
 
     public WebSearch(ToolFactory toolFactory)
         : base(toolFactory)
     {
         apiKey = toolFactory.GetParameter("WebSearch.ApiKey");
-        market = toolFactory.GetParameter("WebSearch.Market") ?? "en-us";
-        safeSearch = toolFactory.GetParameter("WebSearch.SafeSearch") ?? "moderate";
-        baseUrl = toolFactory.GetParameter("WebSearch.BaseUrl") ?? "https://api.bing.microsoft.com/v7.0/search";
+        baseUrl = toolFactory.GetParameter("WebSearch.BaseUrl") ?? "https://api.tavily.com/search";
     }
 
     public override JObject Schema { get; protected set; } = JObject.FromObject(new
@@ -43,11 +37,6 @@ public class WebSearch : Tool
                     {
                         type = "number",
                         description = "The integer number of results to return (default 5)"
-                    },
-                    offset = new
-                    {
-                        type = "number",
-                        description = "The integer offset that specify where to start returning results (default 0)"
                     }
                 },
                 required = new[] { "query" }
@@ -55,77 +44,81 @@ public class WebSearch : Tool
         }
     });
 
-    public override Task<JToken> Function(JObject parameters)
+    public async override Task<JToken> Function(JObject parameters)
     {
         var result = new JObject();
 
         if (string.IsNullOrEmpty(apiKey))
         {
             result.Add("error", "apiKey is null or empty");
-            return Task.FromResult<JToken>(result);
+            return result;
         }
 
         var query = parameters["query"]?.ToString();
         if (string.IsNullOrEmpty(query))
         {
             result.Add("error", "query is null or empty");
-            return Task.FromResult<JToken>(result);
+            return result;
         }
 
         var count = parameters["count"]?.Value<int>() ?? 5;
-        var offset = parameters["offset"]?.Value<int>() ?? 0;
 
         try
         {
             JObject? response = null;
             using (var httpClient = new HttpClient())
             {
-                var uri = $"{baseUrl}?mkt={market}&safeSearch={safeSearch}&count={count}&offset={offset}&q={Uri.EscapeDataString(query)}";
-                var httpRequest = new HttpRequestMessage(HttpMethod.Get, uri);
-                httpRequest.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+                var httpRequest = new HttpRequestMessage(HttpMethod.Post, baseUrl);
+                httpRequest.Headers.Add("Authorization", $"Bearer {apiKey}");
+                httpRequest.Content = new StringContent(JObject.FromObject(new { query, max_results = count }).ToString(), System.Text.Encoding.UTF8, "application/json");
 
                 var httpResponse = httpClient.Send(httpRequest);
                 httpResponse.EnsureSuccessStatusCode();
 
-                var content = httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                var content = await httpResponse.Content.ReadAsStringAsync();
                 if (string.IsNullOrEmpty(content))
                 {
                     result.Add("error", "response is null or empty");
-                    return Task.FromResult<JToken>(result);
+                    return result;
                 }
 
                 response = JObject.Parse(content);
             }
 
-            var webPages = response?.Value<JObject>("webPages")?.Value<JArray>("value");
-            if (webPages == null)
+            if (response == null)
             {
-                result.Add("error", "could not parse search results");
-                return Task.FromResult<JToken>(result);
+                result.Add("error", "could not parse result as json");
+                return result;
+            }
+
+            if (!response.ContainsKey("results") || response.Value<JArray>("results") is not JArray results)
+            {
+                result.Add("error", "could not parse results");
+                return result;
             }
 
             var searchResults = new JArray();
-            foreach (var searchResult in webPages)
+            foreach (var searchResult in results)
             {
-                var name = searchResult.Value<string>("name");
+                var title = searchResult.Value<string>("title");
                 var url = searchResult.Value<string>("url");
-                var snippet = searchResult.Value<string>("snippet");
+                var content = searchResult.Value<string>("content");
 
-                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(url) || string.IsNullOrEmpty(snippet))
+                if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(url) || string.IsNullOrEmpty(content))
                 {
                     continue;
                 }
 
-                searchResults.Add(JObject.FromObject(new { name, url, snippet }));
+                searchResults.Add(JObject.FromObject(new { title, url, content }));
             }
 
-            return Task.FromResult<JToken>(searchResults);
+            return searchResults;
         }
         catch (Exception e)
         {
             result.Add("exception", e.Message);
         }
 
-        return Task.FromResult<JToken>(result);
+        return result;
     }
 }
