9da0129070b4004921ed9fb1cb268096b508c47b
Victor Hong <victor@victorqhong.com>
2025-09-15 15:25:39 -0400

    Update RLArena

--- RLArena/Program.cs
+++ RLArena/Program.cs
@@ -1,14 +1,11 @@
 using LlmAgents.Agents;
 using LlmAgents.Communication;
 using LlmAgents.LlmApi.Content;
-using LlmAgents.State;
 using LlmAgents.Tools;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Parquet;
 using RLArena;
-using System;
+using System.Diagnostics;
 
 using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
 
@@ -16,6 +13,7 @@ var consoleCommunication = new ConsoleCommunication()
 {
     NullOutput = false
 };
+
 var apiEndpoint = "http://llamacpp.home.local:8080/v1/chat/completions";
 var apiKey = "sk-none";
 var apiModel = "llamacpp";
@@ -26,22 +24,9 @@ var workingDirectory = @"C:\Users\victo\Code\agents\agents\rlarena";
 var storageDirectory = Environment.CurrentDirectory; 
 var toolsFilePath = "tools.json";
 States state = States.BuildFail;
-
-//using var fs = System.IO.File.OpenRead(@"C:\Users\victo\Code\LeetCodeDataset\default\train\0000.parquet");
-//using var reader = await ParquetReader.CreateAsync(fs);
-
-//var problemDescriptionDf = reader.Schema.FindDataField("problem_description");
-//var starterCodeDf = reader.Schema.FindDataField("starter_code");
-
-//for (int i = 0; i < reader.RowGroupCount; i++)
-//{
-//    using var rowGroupReader = reader.OpenRowGroupReader(i);
-//    var problemDescriptionData = await rowGroupReader.ReadColumnAsync(problemDescriptionDf);
-//    var starterCodeData = await rowGroupReader.ReadColumnAsync(starterCodeDf);
-//}
+bool resettingEnvironment = false;
 
 var goal = "Create a dotnet console application in C# that writes 'hello world'.";
-//var systemPrompt = $"You are running in an environment where the user will not respond to any of your messages. Pause after each message for input from the user. Use a single tool at a time and pause after each tool used.\n\nThe following commands will be run in the {workingDirectory} directory to build, test, and run the project.\nBuild: dotnet build\nTest: dotnet test\nRun: dotnet run\nThese exact commands will be used to evaluate whether the project is completed.";
 var systemPrompt = $"Goal: {goal}";
 
 var agent = await LlmAgent.CreateAgent(loggerFactory, consoleCommunication, apiEndpoint, apiKey, apiModel, contextSize, maxCompletionTokens, agentId, workingDirectory, storageDirectory, toolsFilePath: toolsFilePath, systemPrompt: systemPrompt);
@@ -52,7 +37,7 @@ var actions = Enum.GetNames<Actions>();
 
 var actionBuffer = new CircularBuffer<Actions>(5);
 
-double[,] qTable = new double[states.Length, actions.Length];
+var qTable = new Dictionary<string, double[]>();
 double alpha = 0.1; // Learning rate
 double gamma = 0.9; // Discount factor
 double epsilon = 0.3; // Exploration rate
@@ -60,88 +45,188 @@ Random rand = new();
 
 agent.ToolEventBus?.SubscribeToolEvent<Tool>(async e =>
 {
+    if (e.Sender.Name.Equals("directory_change"))
+    {
+        var tce = e as ToolCallEvent;
+        workingDirectory = tce.Result.Value<string>("currentDirectory");
+    }
+
+    if (resettingEnvironment)
+    {
+        return;
+    }
+
+    if (state == States.GoalComplete)
+    {
+        return;
+    }
+
     var toolName = e.Sender.Name;
     var action = ToolNameToAction(toolName);
 
     var newState = EvaluateState();
-    var reward = CalculateReward(newState, state, action);
-    qTable[(int)state, (int)action] += alpha * (reward + gamma * MaxQ(newState) - qTable[(int)state, (int)action]);
+    var reward = CalculateReward(newState, state, action, actionBuffer);
+    var (qVals, key) = GetQTableEntry(state, actionBuffer);
+    actionBuffer.Add(action);
+    qVals[(int)action] += alpha * (reward + gamma * MaxQ(newState, actionBuffer) - qVals[(int)action]);
     state = newState;
 });
 
-bool loadResults = false;
+bool loadResults = true;
 bool saveResults = false;
+bool agentMode = false;
 
-if (loadResults && File.Exists("qtable.bin"))
+if (loadResults)
 {
-    var br = new BinaryReader(File.OpenRead("qtable.bin"));
-    for (int i = 0; i < qTable.GetLength(0); i++)
+    LoadQTable("qtable.bin", out qTable);
+}
+
+int episodes = 100;
+int step = 0;
+
+if (agentMode)
+{
+    for (int i = 0; i < episodes; i++)
     {
-        for (int j = 0; j < qTable.GetLength(1); j++)
+        Console.WriteLine($"======================== Episode {i + 1} ========================");
+        ResetEnvironment();
+
+        state = States.BuildFail;
+        actionBuffer = new CircularBuffer<Actions>(5); // Reset buffer each episode
+        while (FeatureIncomplete())
         {
-            qTable[i, j] = br.ReadDouble();
+            if (step >= 60)
+            {
+                break;
+            }
+
+            Console.WriteLine($"======================== Episode {i + 1} Step {step + 1} ========================");
+            await RunAgentAction(state);
+            step++;
+        }
+
+        step = 0;
+
+        agent.Messages.Clear();
+        agent.Messages.Add(JObject.FromObject(new { role = "system", content = systemPrompt }));
+
+        if (saveResults)
+        {
+            SaveQTable("qtable.bin", qTable);
         }
     }
 }
 
-var agentMode = false;
-
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < episodes; i++)
 {
+    state = States.BuildFail;
+
+    Console.WriteLine($"======================== Episode {i + 1} ========================");
     ResetEnvironment();
 
-    epsilon = (1.0 - i / 9.0) - (1.0 - i / 9.0) * 0.5 + 0.1;
+    //epsilon = (1.0 - i / (float)(episodes - 1)) * 0.4 + 0.05;
+    epsilon = 0;
 
-    state = States.BuildFail;
-    while (FeatureIncomplete())
+    actionBuffer = new CircularBuffer<Actions>(5); // Reset buffer each episode
+    while (state != States.GoalComplete)
     {
-        if (agentMode)
+        if (step >= 60)
         {
-            await RunAgentAction(state);
+            break;
+        }
+
+        Actions action;
+        if (rand.NextDouble() < epsilon)
+        {
+            action = Enum.Parse<Actions>(actions[rand.Next(0, actions.Length)]);
         }
         else
         {
-            Actions action;
-            if (rand.NextDouble() < epsilon)
-            {
-                action = Enum.Parse<Actions>(actions[rand.Next(0, actions.Length)]);
-            }
-            else
-            {
-                action = BestAction(state);
-            }
+            action = BestAction(state, actionBuffer);
+        }
 
-            actionBuffer.Add(action);
+        Console.WriteLine($"======================== Episode {i + 1} Step {step + 1} Action {action} ========================");
+        await RunAction(state, action);
 
-            await RunAction(state, action);
-
+        if (action == Actions.think)
+        {
             var newState = EvaluateState();
-            var reward = CalculateReward(newState, state, action);
-            qTable[(int)state, (int)action] += alpha * (reward + gamma * MaxQ(newState) - qTable[(int)state, (int)action]);
+            var reward = CalculateReward(newState, state, action, actionBuffer);
+            var (qVals, key) = GetQTableEntry(state, actionBuffer);
+            actionBuffer.Add(action);
+            qVals[(int)action] += alpha * (reward + gamma * MaxQ(newState, actionBuffer) - qVals[(int)action]);
             state = newState;
         }
+
+        step++;
     }
 
     agent.Messages.Clear();
     agent.Messages.Add(JObject.FromObject(new { role = "system", content = systemPrompt }));
+
+    if (saveResults)
+    {
+        SaveQTable("qtable.bin", qTable);
+    }
 }
 
-if (saveResults)
+SaveQTable("qtable.bin", qTable);
+LoadQTable("qtable.bin", out var _);
+
+Debugger.Launch();
+
+void LoadQTable(string path, out Dictionary<string, double[]> qTable)
 {
-    var bw = new BinaryWriter(File.OpenWrite("qtable.bin"));
-    for (int i = 0; i < qTable.GetLength(0); i++)
+    qTable = [];
+    if (!File.Exists(path))
     {
-        for (int j = 0; j < qTable.GetLength(1); j++)
-        {
-            bw.Write(qTable[i, j]);
-        }
+        return;
     }
 
-    bw.Close();
+    using var br = new BinaryReader(File.OpenRead(path));
+    var count = br.ReadInt32();
+    for (int i = 0; i < count; i++)
+    {
+        var key = br.ReadString();
+
+        var value = new double[actions.Length];
+        for (int j = 0; j < value.Length; j++)
+        {
+            value[j] = br.ReadDouble();
+        }
+
+        if (qTable.ContainsKey(key))
+        {
+            throw new Exception();
+        }
+        else
+        {
+            qTable.Add(key, value);
+        }
+    }
+}
+
+void SaveQTable(string path, Dictionary<string, double[]> qTable)
+{
+    using var bw = new BinaryWriter(File.OpenWrite(path));
+    bw.Write(qTable.Count);
+    foreach (var kvp in qTable)
+    {
+        bw.Write(kvp.Key);
+        for (int j = 0; j < actions.Length; j++)
+        {
+            bw.Write(kvp.Value[j]);
+        }
+    }
 }
 
 void ResetEnvironment()
 {
+    resettingEnvironment = true;
+
+    step = 0;
+    workingDirectory = @"C:\Users\victo\Code\agents\agents\rlarena";
+    agent.FindTool("directory_change")?.Invoke(JObject.FromObject(new { path = workingDirectory }));
     foreach (var directory in Directory.EnumerateDirectories(workingDirectory, "*.*", SearchOption.AllDirectories))
     {
         Directory.Delete(directory, true);
@@ -151,44 +236,84 @@ void ResetEnvironment()
     {
         File.Delete(file);
     }
+
+    resettingEnvironment = false;
 }
 
-double MaxQ(States state)
+string StateToKey(States state, params Actions[] actions)
 {
-    var maxQ = qTable[(int)state, 0];
-    for (int i = 1; i < actions.Length; i++)
+    return $"{state}|{string.Join('|', actions)}";
+}
+
+// Helper for Q-table key
+(double[], string) GetQTableEntry(States state, CircularBuffer<Actions> buffer)
+{
+    var items = buffer.GetItems();
+
+    string key;
+    switch (items.Count)
     {
-        maxQ = Math.Max(maxQ, qTable[(int)state, i]);
+        case 0:
+            key = StateToKey(state);
+            break;
+        case 1:
+            key = StateToKey(state, items[0]);
+            break;
+        case 2:
+            key = StateToKey(state, items[0], items[1]);
+            break;
+        case 3:
+            key = StateToKey(state, items[0], items[1], items[2]);
+            break;
+        case 4:
+            key = StateToKey(state, items[0], items[1], items[2], items[3]);
+            break;
+        case 5:
+            key = StateToKey(state, items[0], items[1], items[2], items[3], items[4]);
+            break;
+        default:
+            throw new Exception();
     }
 
-    return maxQ;
-}
-
-int ArgMaxQ(States state)
-{
-    var maxIndex = 0;
-    var maxQ = qTable[(int)state, maxIndex];
-    for (int i = 1; i < actions.Length; i++)
+    if (!qTable.TryGetValue(key, out var qVals))
     {
-        var q = qTable[(int)state, i];
-        if (q > maxQ)
-        {
-            maxQ = q;
-            maxIndex = i;
-        }
-        else if (Math.Abs(q - maxQ) < 0.001 && rand.NextDouble() < epsilon)
-        {
-            maxQ = q;
-            maxIndex = i;
-        }
+        qVals = new double[actions.Length];
+        qTable[key] = qVals;
     }
 
-    return maxIndex;
+    return (qVals, key);
 }
 
-Actions BestAction(States state)
+double[] GetQValues(States state, CircularBuffer<Actions> buffer)
 {
-  return Enum.Parse<Actions>(actions[ArgMaxQ(state)]);
+    return GetQTableEntry(state, buffer).Item1;
+}
+
+double MaxQ(States state, CircularBuffer<Actions> buffer)
+{
+    var qVals = GetQValues(state, buffer);
+    return qVals.Max();
+}
+
+int ArgMaxQ(States state, CircularBuffer<Actions> buffer)
+{
+    var qVals = GetQValues(state, buffer);
+    double maxQ = qVals[0];
+    int maxIdx = 0;
+    for (int i = 1; i < qVals.Length; i++)
+    {
+        if (qVals[i] > maxQ)
+        {
+            maxQ = qVals[i];
+            maxIdx = i;
+        }
+    }
+    return maxIdx;
+}
+
+Actions BestAction(States state, CircularBuffer<Actions> buffer)
+{
+    return (Actions)ArgMaxQ(state, buffer);
 }
 
 async Task RunAgentAction(States state)
@@ -200,11 +325,11 @@ async Task RunAgentAction(States state)
     }
     else
     {
-        message = "Current state: running 'dotnet_run' did not output 'hello world'";
+        message = "Current state: running 'dotnet_run' did not output exactly 'hello world'";
     }
 
     var content = new MessageContentText() { Text = $"{message}\nUse the most appropriate tool." };
-    await agent.SendMessage([content], agent.Tools, "required");
+    await agent.SendMessage([content], agent.Tools, "auto");
 }
 
 async Task RunAction(States state, Actions action)
@@ -216,15 +341,26 @@ async Task RunAction(States state, Actions action)
     }
     else
     {
-        message = "Current state: running 'dotnet_run' did not output 'hello world'";
+        message = "Current state: running 'dotnet_run' did not output exactly 'hello world'";
     }
 
     var toolName = ActionToToolName(action);
+    List<Tool>? tools = null;
+    string? toolChoice = null;
+    if (action == Actions.think)
+    {
+        tools = null;
+        toolChoice = "auto";
+    }
+    else
+    {
+        var t = agent.Tools.Where(tool => string.Equals(toolName, tool.Name)).ToList();
+        tools = t;
+        toolChoice = "auto";
+    }
 
-    var t = agent.Tools.Where(tool => string.Equals(toolName, tool.Name)).ToList();
     var content = new MessageContentText() { Text = $"{message}\nUse the {toolName} tool." };
-    //var content = new MessageContentText() { Text = JObject.FromObject(new { currentState = state.ToString(), useTool = toolName }).ToString(Formatting.None) };
-    await agent.SendMessage([content], t, "required");
+    await agent.SendMessage([content], tools, toolChoice);
 }
 
 string ActionToToolName(Actions action)
@@ -237,11 +373,37 @@ Actions ToolNameToAction(string toolName)
     return Enum.Parse<Actions>(toolName);
 }
 
-    double CalculateReward(States newState, States oldState, Actions action)
+double CalculateReward(States newState, States oldState, Actions action, CircularBuffer<Actions> buffer)
 {
+    if (newState == States.GoalComplete)
+    {
+        return 1;
+    }
+
+    var items = buffer.GetItems();
+
+    if (newState == States.GoalIncomplete && oldState == States.BuildFail)
+    {
+        return 0.5;
+    }
+
+    if (newState == States.GoalIncomplete && action == Actions.dotnet_run)
+    {
+        return 0.2;
+    }
+
+    if (newState == States.BuildFail && action == Actions.file_write)
+    {
+        return 0.1;
+    }
+
     if (newState == States.BuildFail && oldState == States.GoalIncomplete)
     {
         if (action == Actions.file_delete)
+        {
+            return -0.5;
+        }
+        else if (action == Actions.folder_delete)
         {
             return -0.5;
         }
@@ -251,20 +413,48 @@ Actions ToolNameToAction(string toolName)
 
     if (newState == States.BuildFail)
     {
-        return 0.1;
+        if (action == Actions.file_delete)
+        {
+            return -0.1;
+        }
+        else if (action == Actions.folder_delete)
+        {
+            return -0.1;
+        }
+    }
+
+    if (items.Count > 1 && action == Actions.think && items[^1] == Actions.think)
+    {
+        return -0.5;
+    }
+
+    if (items.Count >= 4 && action == items[^1] && items[^1] == items[^2] && items[^2] == items[^3] && items[^3] == items[^4])
+    {
+        return -0.5;
+    }
+    else if (items.Count >= 3 && action == items[^1] && items[^1] == items[^2] && items[^2] == items[^3])
+    {
+        return -0.3;
+    }
+    else if (items.Count >= 2 && action == items[^1] && items[^1] == items[^2])
+    {
+        return -0.2;
+    }
+    else if (items.Count >= 1 && action == items[^1])
+    {
+        return -0.1;
+    }
+
+    if (newState == States.BuildFail)
+    {
+        return -0.01;
     }
     else if (newState == States.GoalIncomplete)
     {
-        return 0.1;
+        return -0.01;
     }
-    else if (newState == States.GoalComplete)
-    {
-        return 1;
-    }
-    else
-    {
-        throw new Exception();
-    }
+
+    throw new Exception();
 }
 
 States EvaluateState()
@@ -301,7 +491,7 @@ bool TestsFail()
 bool FeatureIncomplete()
 {
     RunCommand("dotnet", "run", workingDirectory, out var stdout);
-    return !string.Equals(stdout.Trim(), "hello world");
+    return !string.Equals(stdout?.Trim(), "hello world");
 }
 
 bool RunCommand(string fileName, string arguments, string? workingDirectory, out string? stdout)
@@ -310,7 +500,6 @@ bool RunCommand(string fileName, string arguments, string? workingDirectory, out
     {
         workingDirectory = Environment.CurrentDirectory;
     }
-
     var process = new System.Diagnostics.Process();
     try
     {
@@ -325,9 +514,7 @@ bool RunCommand(string fileName, string arguments, string? workingDirectory, out
         {
             process.Kill(true);
         }
-
         stdout = process.StandardOutput.ReadToEnd();
-
         return process.ExitCode != 0;
     }
     catch
@@ -346,10 +533,15 @@ enum States
 
 enum Actions
 {
-    file_read = 0,
-    file_write = 1,
-    file_list = 2,
-    file_delete = 3,
-    dotnet_build = 4,
-    dotnet_run = 5,
+    think = 0,
+    file_read = 1,
+    file_write = 2,
+    file_list = 3,
+    file_delete = 4,
+    folder_create = 5,
+    folder_delete = 6,
+    dotnet_build = 7,
+    dotnet_run = 8,
+    directory_change = 9,
+    directory_current = 10,
 }
