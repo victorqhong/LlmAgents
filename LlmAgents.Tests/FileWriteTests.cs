d31885e0514eaf0fccd85db6ab540c1929a5faa4
Victor Hong <victor@victorqhong.com>
2026-02-11 01:09:28 -0500

    Handle more escaped characters by FileWrite tool and add tests (#53)

--- LlmAgents.Tests/FileWriteTests.cs
+++ LlmAgents.Tests/FileWriteTests.cs
@@ -11,12 +11,18 @@ namespace LlmAgents.Tests;
 [TestClass]
 public class FileWriteTests
 {
-    [TestMethod]
-    public async Task TestFileWriteResponse()
+    [DataTestMethod]
+    [DataRow("response1.json", "out1.txt")]
+    [DataRow("response2.json", "out2.txt")]
+    [DataRow("response3.json", "out3.txt")]
+    public async Task TestFileWriteResponse(string responseFile, string fileName)
     {
-        Assert.IsFalse(File.Exists("JobStatusTool.cs"));
+        if (File.Exists(fileName))
+        {
+            File.Delete(fileName);
+        }
 
-        var response = File.ReadAllText("Responses/tools/file_write/response.json");
+        var response = File.ReadAllText($"Responses/tools/file_write/{responseFile}");
         Assert.IsNotNull(response);
 
         var parameters = JObject.Parse(response);
@@ -24,21 +30,15 @@ public class FileWriteTests
         var loggerFactory = new LoggerFactory();
         var toolFactory = new ToolFactory(loggerFactory);
         var tool = new FileWrite(toolFactory);
-        try
-        {
-            var result = await tool.Function(Session.New(), parameters);
-            Assert.IsTrue(File.Exists("JobStatusTool.cs"));
-            
-            var contents = File.ReadAllText("JobStatusTool.cs");
-            Assert.IsFalse(contents.Contains(@"\n"), "File written contains unreplaced newline character");
-        }
-        finally
-        {
-            if (File.Exists("JobStatusTool.cs"))
-            {
-                File.Delete("JobStatusTool.cs");
-            }
-        }
 
+        var result = await tool.Function(Session.New(), parameters);
+        Assert.IsNotNull(result);
+        Assert.IsTrue((result is JObject obj) && string.Equals(obj.Value<string>("result"), "success"));
+        Assert.IsTrue(File.Exists(fileName));
+
+        var contents = File.ReadAllText(fileName);
+        Assert.IsFalse(contents.Contains(@"\"""), "File written contains '\\\"'");
+        Assert.IsFalse(contents.Contains(@"\r\n"), "File written contains '\\r\\n'");
+        Assert.IsFalse(contents.Contains(@"\n"), "File written contains '\\n'");
     }
 }
