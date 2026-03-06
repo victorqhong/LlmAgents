29472d72034d6e9f339cafd62c22707c51be21f7
Victor Hong <victor@victorqhong.com>
2026-03-06 14:41:30 -0500

    Add proxy configuration

--- AgentManager/Program.cs
+++ AgentManager/Program.cs
@@ -1,11 +1,26 @@
+using System.Net;
 using AgentManager.Data;
 using AgentManager.Extensions;
 using AgentManager.Hubs;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContextFactory<AppDbContext>(options => options.UseSqlite("Data Source=agents.db"));
+
+var proxyServerIp = builder.Configuration["Proxy:ServerIp"];
+if (!string.IsNullOrEmpty(proxyServerIp) && IPAddress.TryParse(proxyServerIp, out var ipAddress))
+{
+    builder.Services.Configure<ForwardedHeadersOptions>(options =>
+    {
+        options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedHost;
+        options.KnownNetworks.Clear();
+        options.KnownProxies.Clear();
+        options.KnownProxies.Add(ipAddress);
+    });
+}
+
 builder.Services.AddSignalR();
 
 builder.ConfigureBlazor();
